ed869f76829502fba3052884902cbb63d2817b69
Miss-Rhyme <rimakuma93@gmail.com>
2019-11-26 15:48:18 +0000

    m

--- test_reverse_factorial.py
+++ test_reverse_factorial.py
@@ -1,11 +1,11 @@
 import pytest
 from reverse_factorial import factorial
 
-#def test1():
-#    assert factorial(120) == 5
+def test1():
+    assert factorial(120) == 5
 
 def test2():
-    assert factorial(150) == 'None'
+    assert factorial(720) == 6
 
 def test3():
     assert factorial(24) == 4
