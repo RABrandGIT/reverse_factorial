cd14b4f84f3461b2b8db15a6882296a1ca28c9b3
Miss-Rhyme <rimakuma93@gmail.com>
2019-11-26 15:00:12 +0000

    m

--- reverse_factorial.py
+++ reverse_factorial.py
@@ -3,14 +3,11 @@ def reverse_factorial(x):
     y = x
     for i in range(2,x):
         y = y/i
-        print(y)
         if math.factorial(y)  == x:
-            print("found")
             return y
             break
         
-value = int(input("Enter number: ",))
-print(reverse_factorial(value))
-print("end")
+#value = int(input("Enter number: ",))
+#print(reverse_factorial(value))
 
         
